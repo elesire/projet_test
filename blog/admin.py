80767ca4d12530b43c24d97db60360fdd27482d1
elesire <elesire@free.fr>
2014-10-18 17:56:01 +0200

    removing all

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from blog.models import *
-
-class BilletAdmin(admin.ModelAdmin):
-    list_display = ('id','name','description','photo')
-
-admin.site.register(Billet,BilletAdmin)
--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('blog.views',
-    url(r'^blog/$', 'blog', name='blog'),
-    url(r'^blog/(?P<billet_id>[0-9]+)/$', 'billet', name='billet'),
-)
